7fd381dc5e2af26925b4b2432e701669a5a058bd
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-09-12 09:39:14 +0300

    modified:   fibonacci.js
    Implemented fibonacci array using for loop.

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,4 +1,14 @@
 function fibs(n) {
-    // 
+    // Do the math here
+    let arr = [0, 1];
+    for (let i = 2; i < n; i++) {
+        let z = arr[(i-2)] + arr[(i-1)]
+        arr.push(z)        
+    }
     return arr
-}
+};
+
+console.log(fibs(8));
+console.log(fibs(1));
+console.log(fibs(15));
+
