a0f49e0d9dd7516b3159fb9a5a77901e828d1821
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-09-12 10:10:00 +0300

    modified:   fibonacci.js
    Added fibsRec which returns an array of Nth fibonacci recursivelly

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,7 +8,20 @@ function fibs(n) {
     return arr
 };
 
-console.log(fibs(8));
-console.log(fibs(1));
-console.log(fibs(15));
+function fibsRec(n, arr = [0, 1]) {
+    // Base case
+    // if base case
+        // return arr
+    // Recursive case
+    //return fibsRec(n, arr)
+    if (arr.length >= n) {
+        return arr
+    }
+    arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+    return fibsRec(n, arr);
+}
 
+
+console.log(fibsRec(8));
+console.log(fibsRec(2));
+console.log(fibsRec(15));
